ea69cd68bbfae39707db19a3ddc6a459aeb5d88b
fireduck64 <fireduck@gmail.com>
2021-04-26 23:49:45 -0700

    separate data

--- bin/snow-client-start.sh
+++ bin/snow-client-start.sh
@@ -22,8 +22,8 @@ wallet_vol="$HOME/shard-load-test.git/loadclients/client.${group}"
 docker run -d --restart always --name snow.client --network host \
   -e snow_client_network=$network \
   -e snow_client_node_uri=$uri \
-  -e snow_client_wallet_path=/data \
+  -e snow_client_wallet_path=/wallet \
   -e snow_client_seed_gap=0 \
   -e snow_client_preferred_shard=$shard \
-  -v $wallet_vol:/data $image client loadtest_shard
+  -v $wallet_vol:/wallet $image client loadtest_shard
 
