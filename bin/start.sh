3c0f5880d6623224bacd1d78cfaa5516db2ebcc5
fireduck64 <fireduck@gmail.com>
2021-05-06 07:30:10 -0700

    Add general start command

---
+++ bin/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+host=$(hostname)
+
+if [ "$host" = "seed" ]
+then
+  start-seed.sh
+  exit 0
+fi
+
+
+base=$(echo $host|cut -d "-" -f 1)
+group=$(echo $host|cut -d "-" -f 2)
+shard=$(echo $host|cut -d "-" -f 3)
+
+echo "Base: $base"
+echo "Group: $group"
+echo "Shard: $shard"
+
+if [ "$base" = "node" ]
+then
+  start-group.sh $group $shard
+  start-client.sh a $shard
+  start-client.sh b $shard
+  start-client.sh c $shard
+  start-client.sh d $shard
+fi
+
