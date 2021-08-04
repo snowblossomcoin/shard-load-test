32bae87b671d62ddc6c229772285b5e3a9b38407
fireduck64 <fireduck@gmail.com>
2021-08-03 22:06:31 -0700

    mine

--- single/start.sh
+++ single/start.sh
@@ -7,11 +7,11 @@ start-group.sh z $seed_shard
 
 for group in $group_list
 do
+  start-group-mine.sh $group
   for shard in $node_list
   do
     echo "Starting $group $shard"
     start-group.sh $group $shard
-    start-group-mine.sh $group
     start-client.sh $group $shard
   done
 done
