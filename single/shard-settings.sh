a99b69b67af1cd2b6e7452d331d277dc62fa576a
fireduck64 <fireduck@gmail.com>
2022-06-01 10:59:34 -0700

    Update local shard settings

--- single/shard-settings.sh
+++ single/shard-settings.sh
@@ -9,11 +9,16 @@ load_test_rate=1
 
 miner_rate=8000
 
-image=snowblossom/snowshard
+image=snowblossom/snowblossom
 
 ram="-Xms6g -Xmx6g"
 
 seed_ram="-Xms8g -Xmx8g"
 
 network=demoshard
+
+if which shard-settings.local.sh >> /dev/null
+then
+  . shard-settings.local.sh
+fi
 
